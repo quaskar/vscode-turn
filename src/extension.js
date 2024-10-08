d830bd55cb8881fafc936692b9060aa03418ff01
quaskar <quaskar@gmx.de>
2024-10-08 22:24:09 +0200

    Working Hover Feature
    Design of Info window and token dependent content missing

--- src/extension.js
+++ src/extension.js
@@ -115,24 +115,67 @@ function activate(context) {
   context.subscriptions.push(disposable2);
 
 
-  const hoverProvider = vscode.languages.registerHoverProvider("turn", {
+  /*const hoverProvider = vscode.languages.registerHoverProvider("turn", {
     provideHover(document, position, token) {
       const range = document.getWordRangeAtPosition(position);
       const word = document.getText(range);
 
+      const content = new vscode.MarkdownString(`<span style="color:#000;background-color:#fff;">Howdy there.</span>`);
+      content.appendMarkdown(`<p><b> Some label: <code>${value}</code></b></p>`)
+      content.supportHtml = true;
+
+      content.isTrusted = true;
+
       const referenceInfo = {
-        "STUDY": "A command to study a skill",
+        "STUDY": "STUDY Command<hr>A command to study a skill",
         "unit": "Unit entry point"
       };
 
       if (word in referenceInfo) {
-        return new vscode.Hover(referenceInfo[word]);
+        //return new vscode.Hover(referenceInfo[word]);
+        return new vscode.Hover(content);
       }
       return undefined;
     }
-  });
+  });*/
 
-  context.subscriptions.push(hoverProvider);
+  vscode.languages.registerHoverProvider('turn', {
+    provideHover(document, position, token) {
+        
+        // a markdown table, wrapping in a styled span did not work
+        // had to style each "cell" separately
+        // html entity &nbsp; works
+
+        const markdown = new vscode.MarkdownString(`
+|    <span style="color:#ff0;background-color:#000;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<H1>Table</h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>|    Header     |
+|    :----:    |    :----:     |
+|first cell    |second cell  |
+|third cell    |<span style="color:#f00;background-color:#fff;">&nbsp;&nbsp;fourth cell&nbsp;&nbsp;</span>  |
+        \n\n\n`);  // the newline is necessary for any following appends to work correctly, multiple newlines are reduced to one
+        
+        const styledString = `<span style="color:#fff;background-color:#666;">&nbsp;&nbsp;&nbsp;NASA code follows:&nbsp;&nbsp;&nbsp;</span>`;
+
+        const codeBlock = `const a = 12;
+if (a) return;`;    // any preceding tabs will be rendered in a template literal, so flush left
+
+        // const codeBlock2 = `const c = 12;\nif (c) return;`;  // works, alternate form with newline
+
+        markdown.appendText("______________________________\n");  // a fake separator
+        markdown.appendMarkdown(styledString);
+        markdown.appendCodeblock(codeBlock, "turn");
+        markdown.appendMarkdown(
+`**Bold Text**
+* some note
+* another note
+* final note`
+        );
+
+        markdown.isTrusted = true;
+
+        return new vscode.Hover(markdown, new vscode.Range(position, position));
+    }});
+
+  //context.subscriptions.push(hoverProvider);
 }
 
 function deactivate() {}
